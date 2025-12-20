8b8d7111c7a744fd1ad903ceb90d73ed4bb70498
Ali Mahmoud <alimahmoud235@outlook.com>
2025-12-20 17:35:38 +0200

    Implemented the rest of Logic2P.py

--- Logic/Logic2P.py
+++ Logic/Logic2P.py
@@ -70,4 +70,60 @@ class Logic2P:
             else:
                 pm.append(Position(player1.pos.row, player1.pos.col - 1))
 
-        
+        def goRight():
+            if player1.pos.col == 8 or self.isVwall(player1.pos.row, player1.pos.col):
+                return
+            if player2.pos.row == player1.pos.row and player2.pos.col == player1.pos.col + 1:
+                if player1.pos.col == 7 or self.isVwall(player1.pos.row, player1.pos.col + 1):
+                    if not(player1.pos.row == 0 or self.isHwall(player1.pos.row - 1, player1.pos.col + 1)):
+                        pm.append(Position(player1.pos.row - 1, player1.pos.col + 1))
+                    if not(player1.pos.row == 8 or self.isHwall(player1.pos.row, player1.pos.col + 1)):
+                        pm.append(Position(player1.pos.row + 1, player1.pos.col + 1))
+                else:
+                    pm.append(Position(player1.pos.row, player1.pos.col + 2))
+            else:
+                pm.append(Position(player1.pos.row, player1.pos.col + 1))
+
+        goUp()
+        goLeft()
+        goRight()
+        goDown()
+        return pm
+
+    def isSurrounded(self, player2, player1, des):
+        blocks = []
+        visited = [[False for x in range(9)] for y in range(9)]
+        p = Player(Position(player1.pos.row, player1.pos.col))
+        blocks.append(p.pos)
+        while blocks:
+            p.pos = blocks.pop()
+            temp = self.possibleMoves(p, player2)
+            for t in temp:
+                if not visited[t.row][t.col]:
+                    if t.row == des:
+                        return False
+                    visited[t.row][t.col] = True
+                    blocks.append(t)
+        return True
+
+    def addHwall(self, c1, r1, player2, player1, des):
+        if r1 < 0 or r1 > 7 or c1 < 0 or c1 > 7:
+            return False
+        if self.hwalls[r1][c1 - 1] or self.hwalls[r1][c1] or self.hwalls[r1][c1+1] or self.vwalls[r1][c1]:
+            return False
+        self.hwalls[r1][c1] = True
+        if self.isSurrounded(player1, player2, des) or self.isSurrounded(player2, player1, 0 if des == 8 else 8):
+            self.hwalls[r1][c1] = False
+            return False
+        return True
+
+    def addVwall(self, c1, r1, player2, player1, des):
+        if r1 < 0 or r1 > 7 or c1 < 0 or c1 > 7:
+            return False
+        if self.vwalls[r1 - 1][c1] or self.vwalls[r1][c1] or self.vwalls[r1+1][c1] or self.hwalls[r1][c1]:
+            return False
+        self.vwalls[r1][c1] = True
+        if self.isSurrounded(player1, player2, des) or self.isSurrounded(player2, player1, 0 if des == 8 else 8):
+            self.vwalls[r1][c1] = False
+            return False
+        return True
